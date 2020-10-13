1154712099b59e4cc24450ca8b603de26bc26eb8
Cleiton Arcanjo <cleiton.santos@zenatur.com.br>
2020-10-13 11:33:44 -0300

    Add class service

---
+++ AppFinanceiro/src/app/in-memory-database.ts
@@ -0,0 +1,19 @@
+// tslint:disable-next-line: eofline
+import {InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category-model';
+
+export class InMemoryDatabase implements InMemoryDbService{
+    // tslint:disable-next-line: typedef
+    createDb(){
+        const categories: Category[] = [
+            { id: 1 , name: 'Moradia', description: 'Pagamentos de contas de casa'},
+            { id: 2 , name: 'Saude', description: 'Plano de saude e remédios' },
+            { id: 3 , name: 'Lazer', description: 'Cinema, parques, praia etc' },
+            { id: 4 , name: 'Salario', description: 'Recebimento de salario' },
+            { id: 5 , name: 'Freelas', description: 'Trabalhos como freelancer' },
+        ];
+        // tslint:disable-next-line: semicolon
+        return { categories }
+    }
+// tslint:disable-next-line: eofline
+}
---
+++ AppFinanceiro/src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, observable, throwError } from 'rxjs';
+import { map, catchError , flatMap } from 'rxjs/operators';
+import { Category } from './category-model';
+import { element } from 'protractor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  // tslint:disable-next-line: no-inferrable-types
+  private apiPath: string = 'api/categoeis';
+
+  constructor( private http: HttpClient) { }
+
+
+  getAll(): Observable<Category[]>{
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataCategories)
+    );
+  }
+
+  getById(id: number): Observable<Category>{
+    const url = this.apiPath + '/' + id;
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataCategory)
+    );
+  }
+
+  create(category: Category): Observable<Category>{
+    return this.http.post(this.apiPath, category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataCategory)
+    );
+  } 
+
+  update(category: Category): Observable<Category>{
+    const url = this.apiPath + '/' + category.id;
+
+    return this.http.put(url, Category).pipe(
+      catchError(this.handleError),
+      map(() => category)
+    );
+  }
+
+  delete(id: number): Observable<any>{
+    const url = this.apiPath + '/' + id;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  private jsonDataCategory(jsonData: any): Category{
+    return jsonData as Category;
+  }
+
+  private jsonDataCategories(jsonData: any[]): Category[]{
+    const categories: Category[] = [];
+    // tslint:disable-next-line: no-shadowed-variable
+    jsonData.forEach(element => categories.push(element as Category));
+    return categories;
+  }
+
+  private handleError(error: any): Observable<any>{
+    console.log('Erro na requisição', error);
+     // tslint:disable-next-line: align
+     return throwError(error);
+  }
+}
